d57ace8889024af0be4d0383ca685c4ed240a17a
aastha kapoor <aastha kapoor>
2017-02-01 17:33:28 +0530

    first upload

---
+++ hill.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    char key[10];
+    gets(key);
+    int len =strlen(key);
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    int arr[][n];
+    for(int i=0;i>n;i+)
+    {
+        for(int j=0;j<n;j++)
+        {
+          cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+
+
+    }
+}
